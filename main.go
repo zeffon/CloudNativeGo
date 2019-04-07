f7803ac8525f88723067a03e3d3d655512093e04
ZefengYu <513447064@qq.com>
2019-04-07 20:59:13 +0800

    ajust go code

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
+    "html"
+    "log"
+    "net/http"
 )
 
 func main() {
-	fmt.Println("hello world")
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    })
+
+    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Hi")
+    })
+
+    log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
